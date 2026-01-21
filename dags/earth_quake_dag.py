1c63a1c787e1f00bad46cdeebfff90e4d405e671
awaisajaz1 <awaisajaz1@gmail.com>
2026-01-21 19:09:04 +0500

    dag

--- dags/earth_quake_dag.py
+++ dags/earth_quake_dag.py
@@ -11,3 +11,11 @@ default_args = {
     'retries' : 1,
     'retry_delay' : timedelta(minutes=5)
 }
+
+
+dag = DAG(
+    'featch_earth_quake_api_data',
+    default_args = default_args,
+    description = 'DAG for earth quakes api to store in postgres',
+    schedule_interval = timedelta(days=1)
+)
