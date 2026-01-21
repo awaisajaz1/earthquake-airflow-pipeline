9ffdeeae32d80216e7cd2735ceacc2c2efd0779e
awaisajaz1 <awaisajaz1@gmail.com>
2026-01-21 23:24:39 +0500

    data is airflwoing

--- dags/earth_quake_dag.py
+++ dags/earth_quake_dag.py
@@ -6,20 +6,135 @@ from airflow import DAG
 from datetime import datetime, timedelta
 import json
 import requests
-import pandas
-from bs4 import BeautifulSoup
 
 from airflow.operators.python import PythonOperator
-from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+# 1
+def fetch_earth_quake_data(ti):
+    url = 'https://earthquake.usgs.gov/fdsnws/event/1/query'
+    params = {
+        "format": "geojson",
+        "starttime": (datetime.utcnow() - timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S"),
+        "endtime": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S"),
+        "minmagnitude": 2.5,
+        "limit": 100  # limit for example, can be adjusted
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    # now convert the responce to json data type 
+    data = response.json()
+
+    # extract the features (earthquake events) from the geojson data
+    records = []
+    for feature in data['features']:
+        earthquake_id = feature['id']
+        # extract the properties and geometry from each feature
+        properties = feature['properties']
+        geometry = feature['geometry']
+
+        # extract the relevant fields from properties
+        place = properties['place']
+        magnitude = properties['mag']
+        time = properties['time']
+
+        # extract the coordinates from geometry
+        coordinates = geometry['coordinates']
+
+        record = {
+            'id': earthquake_id,
+            'place': place,
+            'magnitude': magnitude,
+            'time': datetime.utcfromtimestamp(time/1000).strftime('%Y-%m-%d %H:%M:%S') if time else None,
+            'longitude': coordinates[0],
+            'latitude': coordinates[1],
+            'depth': coordinates[2]
+        }
+
+        records.append(record)
+
+        print("The Earth Quake data has been extracted!")
+
+    # df = pd.Dataframe(records)
+    ti.xcom_push(key='earth_quake_data', value=records)
+
+
+# 2 Insert data to postgres with minor cleansing
+def insert_earthquake_data_to_postgres(ti):
+    earthquake_data = ti.xcom_pull(key='earth_quake_data', task_ids='fetch_earth_quake_data')
+
+    if not earthquake_data:
+        raise ValueError("No earthquake data found")
+
+    postgres_hook = PostgresHook(postgres_conn_id='earth_quake')
+
+    create_table = """CREATE TABLE IF NOT EXISTS earth_quake_data (
+        id VARCHAR(255) PRIMARY KEY,
+        place VARCHAR(255),
+        magnitude FLOAT,
+        time TIMESTAMP,
+        longitude FLOAT,
+        latitude FLOAT,
+        depth FLOAT
+    );
+    """
+    # execute create script
+    postgres_hook.run(create_table)
+    
+    # insert records
+    insert_query = """
+            INSERT INTO earth_quake_data (id, place, magnitude, time, longitude, latitude, depth)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+    for row in earthquake_data:
+        postgres_hook.run(insert_query, parameters=(
+            row['id'],
+            row['place'],
+            row['magnitude'],
+            row['time'],
+            row['longitude'],
+            row['latitude'],
+            row['depth']
+        ))
+
+
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 6, 20),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
 # Simple DAG definition
 dag = DAG(
     dag_id='fetch_earth_quake_api_data',
+    default_args=default_args,
     start_date=datetime(2026, 1, 21),
     schedule=timedelta(days=1),
     description='DAG for earth quakes api to store in postgres',
     catchup=False
 )
 
+fetch_earthquake_data = PythonOperator(
+    task_id='fetch_earth_quake_data',
+    python_callable=fetch_earth_quake_data,
+    dag=dag,
+)
 
+
+
+insert_earthquake_data = PythonOperator(
+    task_id='insert_earth_quake_data_to_postgres',
+    python_callable=insert_earthquake_data_to_postgres,
+    dag=dag,
+)
+
+
+# set dependecies
+fetch_earthquake_data >> insert_earthquake_data
--- test.py
+++ test.py
@@ -1,34 +1,21 @@
+from datetime import datetime, timedelta
+import json
 import requests
 
-base_url = f"https://www.amazon.com/s?k=data+engineering+books"
 
-books = []
-seen_titles = set()  # To keep track of seen titles
-headers = {
-    "Referer": 'https://www.amazon.com/',
-    "Sec-Ch-Ua": "Not_A Brand",
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": "macOS",
-    'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
+params = {
+    "format": "geojson",
+    "starttime": (datetime.utcnow() - timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S"),
+    "endtime": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S"),
+    "minmagnitude": 2.5,
+    "limit": 100  # limit for example, can be adjusted
 }
 
-page = 1
-
-while len(books) < 2:
-    url = f"{base_url}&page={page}"
-    
-    # Send a request to the URL
-    response = requests.get(url, headers=headers)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        print(response)
+response = requests.get(url, params=params)
+response.raise_for_status()
 
 
-        page += 1
-    
-    else:
-
-        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
-
-        break
+# now convert the responce to json data type 
+data = response.json()
+print(data)
