54049aae8aca4a194aed6fdb76dbe357b894b90c
awaisajaz1 <awaisajaz1@gmail.com>
2026-02-03 22:37:31 +0500

    auto xcom by returning valued by task to next task

--- dags/modern_Dag.py
+++ dags/modern_Dag.py
@@ -18,15 +18,30 @@ def modern_dag_automation():
         return transformed_data * 2
     
     @task.python
-    def third_task(transformed_data: list):
+    def third_task(transformed_data: list, **kwargs):
         print(transformed_data)
+        ti = kwargs.get("ti")
+        # Manual XCOM push
+        ti.xcom_push(key = "transformed_data", value = transformed_data)
+        # Don't return anything (or return None) to break automatic XCom chain
+    
+    @task.python
+    def fourth_task(**kwargs):
+        ti = kwargs.get("ti")
+        transformed_data = ti.xcom_pull(key = "transformed_data")
+        print(transformed_data, "Owais this is manual xcom")
+        return transformed_data
+
+
 
 
     # define tasks dependencies
     first_task = first_task()
     second_task = second_task(first_task)
     third_task = third_task(second_task)
+    fourth_task = fourth_task()
 
+    third_task >> fourth_task
 modern_dag_automation()
 
 
